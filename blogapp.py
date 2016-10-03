62d7189d2d1efc992f7702061c1d6f45390c4956
Josh Nichols <josh.nichols.lives@gmail.com>
2016-10-03 17:46:54 -0600

    heroku ready

--- blogapp.py
+++ blogapp.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from models import Base, Post
@@ -15,6 +15,29 @@ app = Flask("__name__")
 @app.route("/")
 def index():
     return render_template("index.html")
+
+@app.route("/posts")
+def posts():
+    posts = session.query(Post).all()
+    return render_template("posts.html", posts=posts)
+
+@app.route("/posts/<int:post_id>")
+def show_post(post_id):
+    post = session.query(Post).filter_by(id=post_id).one()
+    return render_template("show_post.html", post=post)
+
+@app.route("/posts/new", methods = ["GET", "POST"])
+def new_post():
+    print request
+    if request.method == "POST":
+        newPost = Post(content=request.form['content'],
+                    title=request.form['title'])
+        session.add(newPost)
+        session.commit()
+        return redirect(url_for("posts"))
+    else:
+        return render_template("new_post.html")
+
 
 if __name__ == "__main__":
     app.run(debug=config.development["DEBUG"],
--- models.py
+++ models.py
@@ -7,10 +7,17 @@ import config
 
 Base = declarative_base()
 
+class User(Base):
+    __tablename__ = "user"
+    name = Column(String(250), nullable = False)
+    id = Column(Integer, primary_key=True)
+
 class Post(Base):
     __tablename__ = "post"
     content = Column(String(1000), nullable = False)
     title = Column(String(250), nullable = False)
+    user_id = Column(Integer, ForeignKey('user.id'))
+    user = relationship(User)
     id = Column(Integer, primary_key = True)
 
 engine = create_engine(config.DB_URI)
